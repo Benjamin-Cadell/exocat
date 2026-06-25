9c6fe577ad1eae134377ad45fdda247caf37b6d8
benmac <benjamincadell@gmail.com>
2026-06-25 10:40:10 +0100

    Add initial docs RTD template

---
+++ docs/conf.py
@@ -0,0 +1,49 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../src'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'exocat'
+copyright = '2026, Ben Cadell, Melissa Azombo, Khang Nguyen'
+author = 'Ben Cadell, Melissa Azombo, Khang Nguyen'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc",
+              "sphinx.ext.napoleon",
+              "sphinx.ext.doctest",
+              "sphinx.ext.viewcode",
+              "sphinx.ext.autosummary"
+              ]
+
+autosummary_generate = True
+autodoc_default_options = {
+    'members': True,
+    'inherited-members': True,
+    'undoc-members': True,
+}
+autoclass_content = "both"
+autodoc_member_order = "bysource"
+
+autodoc_typehints = "signature"
+autodoc_typehints_format = "short"
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
