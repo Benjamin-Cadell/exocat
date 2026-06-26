e4eed9fbadb9256653682958c5637598ea1e1085
benmac <benjamincadell@gmail.com>
2026-06-26 14:11:29 +0100

    Add new files to init

--- src/exocat/__init__.py
+++ src/exocat/__init__.py
@@ -1,4 +1,7 @@
 from .density import BulkDensity
 from .temperature import Temperature
+from .mass import Mass
+from .read_tepcat_csv import * # tepcat_csv and tepcat_tidy
+from .plots import Plot
 
 __version__ = "0.1.0"
